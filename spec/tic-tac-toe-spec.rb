703797174f84dd4d3063551977ae04ed3d8a2ce3
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-09-29 02:17:29 -0500

    refactor: using tdd to clean up this mess

---
+++ spec/tic-tac-toe-spec.rb
@@ -0,0 +1,26 @@
+require_relative '../tic-tac-toe.rb'
+
+describe Game do
+  describe '#play_round' do
+    context 'when human has 3 in a row play ends' do
+      let(:test_board) { instance_double(Board) }
+      let(:test_human) { instance_double(Player) }
+      let(:test_comp) { instance_double(Computer) }
+      let(:game) { described_class.new(test_board, test_human, test_comp) }
+      before do
+        # board = game.instance_variable_get(:@board)
+        # human = game.instance_variable_get(:@player1)
+        # comp = game.instance_variable_get(:@player2)
+        # allow(:system).to receive('clear')
+        allow(test_board).to receive(:display_board)
+        allow(test_human).to receive(:turn_action)
+        allow(test_human).to receive(:winner?).and_return(true)
+      end
+
+      it 'stops loop and does not check if the board is full' do
+        expect(test_board).not_to receive(:board_full?)
+        game.play_round
+      end
+    end
+  end
+end
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,4 +1,6 @@
-require "pry"
+# frozen_string_literal: true
+
+require 'pry'
 
 module Display
   def prompt(input)
@@ -8,7 +10,7 @@ end
 
 class Board
   include Display
-  WINNING_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  WINNING_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
   attr_reader :size, :valid_positions
   attr_accessor :board_positions
 
@@ -18,51 +20,51 @@ class Board
   end
 
   def calc_valid_positions
-    self.board_positions.select { |el| (1..(size ** 2)).include?(el) }
+    board_positions.select { |el| (1..(size ** 2)).include?(el) }
   end
 
   def display_middle_separator
-    puts "|     |     |     | "
-    puts "-------------------"
-    puts "|     |     |     | "
+    puts '|     |     |     | '
+    puts '-------------------'
+    puts '|     |     |     | '
   end
 
   def display_top_separator
-    puts ""
-    puts "-------------------"
-    puts "|     |     |     | "
+    puts ''
+    puts '-------------------'
+    puts '|     |     |     | '
   end
 
   def display_bottom_separator
-    puts "|     |     |     | "
-    puts "-------------------"
-    puts ""
+    puts '|     |     |     | '
+    puts '-------------------'
+    puts ''
   end
 
   def display_boxes(s1, s2, s3)
     boxes = [s1, s2, s3]
     boxes.each do |s|
-      if s.is_a?(Integer)
-        boxes[boxes.index(s)] = "[" + s.to_s + "]"
-      else
-        boxes[boxes.index(s)] = " " + s + " "
-      end
+      boxes[boxes.index(s)] = if s.is_a?(Integer)
+          "[#{s}]"
+        else
+          " #{s} "
+        end
     end
     puts "| #{boxes[0]} | #{boxes[1]} | #{boxes[2]} |"
   end
 
   def display_board
-    display_top_separator()
+    display_top_separator
     display_boxes(@board_positions[0], @board_positions[1], @board_positions[2])
-    display_middle_separator()
+    display_middle_separator
     display_boxes(@board_positions[3], @board_positions[4], @board_positions[5])
-    display_middle_separator()
+    display_middle_separator
     display_boxes(@board_positions[6], @board_positions[7], @board_positions[8])
-    display_bottom_separator()
+    display_bottom_separator
   end
 
   def board_full?
-    self.calc_valid_positions.empty?
+    calc_valid_positions.empty?
   end
 
   def reset_board
@@ -75,49 +77,47 @@ class Player
   attr_accessor :board, :symbol, :first_move, :winner, :name, :score
 
   def initialize(board)
-    @name = ""
+    @name = ''
     @score = 0
     @first_move = false
     @board = board
-    @symbol = "X"
+    @symbol = 'X'
     @winner = false
   end
 
   def get_name
-    prompt "What is your name?"
+    prompt 'What is your name?'
     choice = gets.chomp
     self.name = choice
   end
 
   def go_first
     self.first_move = true
-    self.symbol = "X"
+    self.symbol = 'X'
   end
 
   def turn_action
-    prompt "Choose an open position on the board #{self.board.calc_valid_positions}"
+    prompt "Choose an open position on the board #{board.calc_valid_positions}"
     loop do
       choice = gets.chomp.to_i
-      position = self.board.board_positions.index(choice)
+      position = board.board_positions.index(choice)
       if valid_action?(choice)
-        self.board.board_positions[position] = self.symbol
+        board.board_positions[position] = symbol
         break
       else
-        prompt "Invalid input: Choose a valid position #{self.board.calc_valid_positions}"
+        prompt "Invalid input: Choose a valid position #{board.calc_valid_positions}"
       end
     end
   end
 
   def valid_action?(move)
-    self.board.calc_valid_positions.include?(move)
+    board.calc_valid_positions.include?(move)
   end
 
   def winner?
-    positions = self.board.board_positions
+    positions = board.board_positions
     Board::WINNING_POSITIONS.each do |group|
-      if group.all? { |i| positions[i - 1] == self.symbol }
-        return self.winner = true
-      end
+      return self.winner = true if group.all? { |i| positions[i - 1] == symbol }
     end
     false
   end
@@ -135,13 +135,13 @@ class Computer < Player
 
   def initialize(board)
     super
-    @symbol = "O"
-    @name = "Computer "
+    @symbol = 'O'
+    @name = 'Computer '
   end
 
   def turn_action
-    choice = self.board.calc_valid_positions.sample
-    self.board.board_positions[choice - 1] = self.symbol
+    choice = board.calc_valid_positions.sample
+    board.board_positions[choice - 1] = symbol
   end
 end
 
@@ -156,83 +156,80 @@ class Game
   end
 
   def welcome
-    system("clear")
-    prompt "Welcome to Tic-Tac-Toe, lets play!"
+    system('clear')
+    prompt 'Welcome to Tic-Tac-Toe, lets play!'
   end
 
   def play_round
     loop do
-      system("clear")
-      self.board.display_board
-      self.player1.turn_action
-      if self.player1.winner? || self.board.board_full?
-        break
-      end
-      self.player2.turn_action
-      if self.player2.winner? || self.board.board_full?
-        break
-      end
+      system('clear')
+      board.display_board
+      player1.turn_action
+      break if player1.winner? || board.board_full?
+
+      player2.turn_action
+      break if player2.winner? || board.board_full?
     end
-    system("clear")
-    self.board.display_board
+    system('clear')
+    board.display_board
   end
 
   def display_round_outcome
-    if self.player1.winner
-      prompt "#{self.player1.name} Wins!"
-    elsif self.player2.winner
-      prompt "#{self.player2.name} Wins!"
+    if player1.winner
+      prompt "#{player1.name} Wins!"
+    elsif player2.winner
+      prompt "#{player2.name} Wins!"
     else
-      prompt "Cat Scratch"
+      prompt 'Cat Scratch'
     end
   end
 
   def play_again?
-    prompt "Would you like to play again? ( yes | no )"
+    prompt 'Would you like to play again? ( yes | no )'
     loop do
       choice = gets.chomp.downcase
       if valid_choice?(choice)
-        if choice == "yes" || choice == "y"
+        case choice
+        when 'yes', 'y'
           return true
-        elsif choice == "no" || choice == "n"
+        when 'no', 'n'
           return false
         end
       end
-      prompt "Invalid input. Valid options: yes, y, no, n"
+      prompt 'Invalid input. Valid options: yes, y, no, n'
     end
   end
 
   def valid_choice?(choice)
-    %w(yes y no n).include?(choice)
+    %w[yes y no n].include?(choice)
   end
 
   def display_game_over
-    prompt "Thanks for playing Tic-Tac-Toe, Goodbye #{self.player1.name}!"
+    prompt "Thanks for playing Tic-Tac-Toe, Goodbye #{player1.name}!"
   end
 
-  def display_player_symbols
-  end
+  def display_player_symbols; end
 end
 
-board = Board.new
-human = Player.new(board)
-computer = Computer.new(board)
-tic_tac_toe = Game.new(board, human, computer)
+# board = Board.new
+# human = Player.new(board)
+# computer = Computer.new(board)
+# tic_tac_toe = Game.new(board, human, computer)
 
-tic_tac_toe.welcome
-human.get_name
+# tic_tac_toe.welcome
+# human.get_name
 
-loop do
-  tic_tac_toe.play_round
-  tic_tac_toe.display_round_outcome
+# loop do
+#   tic_tac_toe.play_round
+#   tic_tac_toe.display_round_outcome
 
-  unless tic_tac_toe.play_again?
-    break
-  else
-    human.reset_player
-    computer.reset_player
-    board.reset_board
-  end
-end
+#   if tic_tac_toe.play_again?
+#     human.reset_player
+#     computer.reset_player
+#     board.reset_board
+#   else
+#     break
+#   end
+# end
 
-tic_tac_toe.display_game_over
+# tic_tac_toe.display_game_over
